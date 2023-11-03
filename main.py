93902fd8c2b155bdae21c013f87a2df25eb68762
William Hou <whou125@gmail.com>
2023-11-02 22:18:28 -0700

    Define endpoints

--- main.py
+++ main.py
@@ -1,23 +1,53 @@
 from fastapi import FastAPI
+from database import User
+from pydantic import BaseModel
 
 # Create our FastAPI instance
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+
 # Get all the users in our database
 @app.get("/users")
 async def get_users():
-    pass
+    users = []
+
+    for user in User.objects:
+        users.append({"email": user.email, "name": user.name})
+
+    return users
+    
 
 # Add a user to the database
+
+# Define the expected body of the request
+class User_Body(BaseModel):
+    email: str
+    name: str
+
 @app.post("/users")
-async def add_user():
-    pass
+async def add_user(u: User_Body):
+    new_user = User(email=u.email, name=u.name)
+
+    # Save the user to the database
+    new_user.save()
+
+    # Return the user's email and name
+    return {"email": new_user.email, "name": new_user.name}
+
 
 # Get a specific user in our database by their email
 @app.get("/users/{email}")
 async def get_user(email: str):
-    pass
+    # Find the user in our database
+    found_user = User.objects(email=email).first()
+
+    # If the user is not found, return an error message
+    if found_user is None:
+        return {"message": "User not found"}
+    else:
+        return {"email": found_user.email, "name": found_user.name}
